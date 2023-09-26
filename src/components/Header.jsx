d7acf74a87687b2013e3b1be1c8cf466cea290c4
Özgür Okan Özdal <ozgurokanozdal@gmail.com>
2023-09-26 13:20:06 +0300

    header changes and some css changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,14 @@ import Covid from "../assets/Covid"
 
 function Header() {
   return (
+    <div className="headerWrapper">
     <div className='header'>C<span><Covid/></span>VID</div>
+    <div className="header-text">
+      <div className="header-text-inner1">Global and Country Wise Cases of Corona Virus</div>
+      <div className="header-text-inner2">(For a Particlar select a Country from below)</div>
+    </div>
+    </div>
+    
   )
 }
 
