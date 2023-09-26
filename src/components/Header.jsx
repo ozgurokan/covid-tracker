d436a1e71df6d608644c0c10ee3f1c9acb495658
Özgür Okan Özdal <ozgurokanozdal@gmail.com>
2023-09-26 13:26:04 +0300

    error massage added to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,6 +8,7 @@ function Header() {
     <div className="header-text">
       <div className="header-text-inner1">Global and Country Wise Cases of Corona Virus</div>
       <div className="header-text-inner2">(For a Particlar select a Country from below)</div>
+      <div className='header-issue-note'>Access to XMLHttpRequest at 'apiLink' from origin 'siteLink' has been blocked by CORS policy: No 'Access-Control-Allow-Origin' header is present on the requested resource.</div>
     </div>
     </div>
     
